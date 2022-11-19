1fe82662b88853358f0773fd540471b62e75d9ec
Jannik Maag <jannikmaag@outlook.com>
2022-11-19 22:05:42 +0100

    Added storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
-
+require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
@@ -8,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
+    'plugin:storybook/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -21,4 +21,4 @@ module.exports = {
       },
     ],
   },
-};
+}
