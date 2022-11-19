60acf013eb01ca76259abb2ed5da3d8d1d15707e
Jannik Maag <jannikmaag@outlook.com>
2022-11-19 21:51:07 +0100

    Remove scaffolded files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h3>Jannik Maag</h3>
+    <p>Software developer at Telia Company.</p>
+    <p>Passionate about Front-End architecture.</p>
+    <p>Freelancer.</p>
+    <p>A human being.</p>
   </main>
 </template>
+
+<script setup lang="ts"></script>
