df4bee9d038f465864baef07e3b2f55c0d8d88d9
Jannik Maag <jannikmaag@outlook.com>
2022-11-20 18:21:59 +0100

    Added global scss files for storybook using viteFinal

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,35 @@
+// use `mergeConfig` to recursively merge Vite options
+const { mergeConfig } = require('vite')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  async viteFinal(config, { configType }) {
+    // Return a custom config
+    // https://github.com/storybookjs/builder-vite#customize-vite-config
+    return mergeConfig(config, {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `
+              @import './src/assets/styles/_spacing.scss';
+              @import './src/assets/styles/_colors.scss';
+              @import 'include-media';
+            `,
+          },
+        },
+      },
+    })
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
 }
