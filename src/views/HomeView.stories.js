f35e4a80b1a1f5977743b17ed0dcb89abf745d09
Jannik Maag <jannikmaag@outlook.com>
2022-11-26 19:48:13 +0100

    Prevent button click navigation in storybook

--- src/views/HomeView.stories.js
+++ src/views/HomeView.stories.js
@@ -15,7 +15,16 @@ const Template = (args) => ({
   template: '<HomeView v-bind="args" />',
 })
 
+const mockContentModel = {
+  ...contentModel,
+  buttonOptions: contentModel.buttonOptions.map((buttonOption) => ({
+    ...buttonOption,
+    // Remove links to prevent navigating when button is click
+    externalLink: null,
+  })),
+}
+
 export const Primary = Template.bind({})
 Primary.args = {
-  content: contentModel,
+  content: mockContentModel,
 }
