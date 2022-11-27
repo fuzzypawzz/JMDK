4ba4f4b647147da9ea93ed57f2bc89b24d61cef5
Jannik Maag <jannikmaag@outlook.com>
2022-11-27 20:41:43 +0100

    Added dark mode styles to switch

--- src/components/theme-switch/theme-switch.vue
+++ src/components/theme-switch/theme-switch.vue
@@ -1,17 +1,22 @@
 <template>
-  <label class="theme-switch">
-    <input
-      class="theme-switch__checkbox"
-      aria-label="Toggle dark mode on/off"
-      role="switch"
-      type="checkbox"
-      :aria-checked="darkModeOn"
-      :checked="darkModeOn"
-      @change="toggleDarkMode()"
-    />
+  <div style="display: inline-flex; border-radius: 6px; padding: 6px 8px">
+    <span style="font-size: 16px; font-weight: 400; margin-right: 6px"
+      >Darkmode</span
+    >
+    <label class="theme-switch">
+      <input
+        class="theme-switch__checkbox"
+        aria-label="Toggle dark mode on/off"
+        role="switch"
+        type="checkbox"
+        :aria-checked="darkModeOn"
+        :checked="darkModeOn"
+        @change="toggleDarkMode()"
+      />
 
-    <span class="theme-switch__slider"></span>
-  </label>
+      <span class="theme-switch__slider"></span>
+    </label>
+  </div>
 </template>
 
 <script lang="ts">
