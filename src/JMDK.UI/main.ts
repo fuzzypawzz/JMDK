f3b062628157395121f4efb0f1b6c0c994135fcc
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-04-18 19:40:43 +0200

    Moved app file to app folder

--- src/JMDK.UI/main.ts
+++ src/JMDK.UI/main.ts
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
-import App from '@/JMDK.UI/App.vue'
+import App from '@/JMDK.UI/components/app/App.vue'
 import router from '@/JMDK.Core/router'
 
 import '@/JMDK.UI/styles/main.scss'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
