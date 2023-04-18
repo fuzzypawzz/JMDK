61e5ecf0dee641cbcec79f1fce5753719f32f9c8
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-04-19 00:04:06 +0200

    Upgraded storybook to latest

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,37 @@
-const path = require('path')
+const path = require('path');
 
 // use `mergeConfig` to recursively merge Vite options
-const { loadConfigFromFile, mergeConfig } = require('vite')
-
+const {
+  loadConfigFromFile,
+  mergeConfig
+} = require('vite');
 module.exports = {
-  async viteFinal(config, { configType }) {
-    const { config: userConfig } = await loadConfigFromFile(
-      path.resolve(__dirname, '../vite.config.ts')
-    )
+  async viteFinal(config, {
+    configType
+  }) {
+    const {
+      config: userConfig
+    } = await loadConfigFromFile(path.resolve(__dirname, '../vite.config.ts'));
 
     // Return a custom config
     // https://github.com/storybookjs/builder-vite#customize-vite-config
     // https://github.com/storybookjs/builder-vite/issues/85
     return mergeConfig(config, {
       ...userConfig,
-      plugins: [],
-    })
+      plugins: []
+    });
   },
   staticDirs: ['../src/JMDK.UI/static'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
-  framework: '@storybook/vue3',
-  core: {
-    builder: '@storybook/builder-vite',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {}
   },
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
