9333751ecfdc98150cc9e2770b70f385bfc07fd6
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-04-18 19:05:06 +0200

    Moved router to Core

--- src/JMDK.Core/router/index.ts
+++ src/JMDK.Core/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { contentModel as homeViewContentModel } from '@/views/HomeViewContentModel'
+import HomeView from '@/JMDK.UI/views/HomeView.vue'
+import { contentModel as homeViewContentModel } from '@/JMDK.UI/views/HomeViewContentModel'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
