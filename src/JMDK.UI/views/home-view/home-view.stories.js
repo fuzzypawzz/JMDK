b65adb7f008f5cc16f421cde0574e7338a62beb0
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-05-03 21:34:37 +0200

    Removed redundant content prop in story

--- src/JMDK.UI/views/home-view/home-view.stories.js
+++ src/JMDK.UI/views/home-view/home-view.stories.js
@@ -1,8 +1,7 @@
 import HomeView from '@/JMDK.UI/views/home-view/home-view.vue'
-import { contentModel } from '@/JMDK.UI/views/home-view/content/default-content'
 
 export default {
-  title: 'Pages/HomeView',
+  title: 'pages/home-view',
   component: HomeView,
   argTypes: {},
 }
@@ -15,16 +14,5 @@ const Template = (args) => ({
   template: '<HomeView v-bind="args" />',
 })
 
-const mockContentModel = {
-  ...contentModel,
-  buttonOptions: contentModel.buttonOptions.map((buttonOption) => ({
-    ...buttonOption,
-    // Remove links to prevent navigating when button is click
-    externalLink: null,
-  })),
-}
-
 export const Primary = Template.bind({})
-Primary.args = {
-  content: mockContentModel,
-}
+Primary.args = {}
