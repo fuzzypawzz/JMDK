b506b96671fa10bfa15151956cab6ccf22f23641
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-08-07 23:32:01 +0200

    Set window height css varaible with js

---
+++ src/JMDK.UI/infrastructure/helpers/browser/window-height.ts
@@ -0,0 +1,19 @@
+const WINDOW_HEIGHT_CSS_VAR = '--window-height'
+
+export function useWindowHeightVariable() {
+  function setWindowHeightVariable() {
+    document.documentElement.style.setProperty(
+      WINDOW_HEIGHT_CSS_VAR,
+      `${window.innerHeight / 100}px`
+    )
+  }
+
+  setWindowHeightVariable()
+  window.addEventListener('resize', setWindowHeightVariable)
+  window.addEventListener('orientationchange', setWindowHeightVariable)
+
+  return function dispose() {
+    window.removeEventListener('resize', setWindowHeightVariable)
+    window.removeEventListener('orientationchange', setWindowHeightVariable)
+  }
+}
