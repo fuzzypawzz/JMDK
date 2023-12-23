79398ca64ded34cff0b34da36438f796a160dc21
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-12-24 00:14:57 +0100

    Wait for promise microtasks for resolve between tests

--- src/JMDK.Core/config/vitest.config.ts
+++ src/JMDK.Core/config/vitest.config.ts
@@ -1,6 +1,10 @@
 import { enableAutoUnmount, flushPromises } from '@vue/test-utils'
 import { afterEach } from 'vitest'
 
+async function waitForNextNodeProcessTick() {
+  await new Promise(process.nextTick)
+}
+
 async function bootStrapApplicationForTesting() {
   // Making sure that components are removed from the DOM 'afterEach' test.
   // This cleanup is relevant for the entire app.
@@ -8,3 +12,13 @@ async function bootStrapApplicationForTesting() {
 }
 
 await bootStrapApplicationForTesting()
+
+afterEach(async () => {
+  // Pause the async function execution until the next process tick,
+  // which allows other pending operations to complete before moving
+  // on to the next test.
+  await waitForNextNodeProcessTick()
+
+  // Wait for all micro tasks to complete.
+  await flushPromises()
+})
