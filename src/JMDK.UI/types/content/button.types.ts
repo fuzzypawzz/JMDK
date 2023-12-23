72e5281c290e1272d9c9dbd4fd120574e25d2343
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-12-24 00:13:10 +0100

    Removed redundant type files since I redid the types around the view model in the presenter

--- src/JMDK.UI/types/content/button.types.ts
+++
@@ -1,8 +0,0 @@
-import type { BUTTON_VARIANT } from '@/JMDK.UI/components/j-button/j-button.constants'
-
-export type _Button = {
-  text: string
-  externalLink?: string
-  title?: string
-  variant?: BUTTON_VARIANT
-}
