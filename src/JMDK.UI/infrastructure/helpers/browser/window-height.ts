256f9b771cf51c577e778ee3bb49b8a5f0afddff
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-12-24 00:13:27 +0100

    Renamed function name to be a little clearer around it being a CSS variable helper

--- src/JMDK.UI/infrastructure/helpers/browser/window-height.ts
+++ src/JMDK.UI/infrastructure/helpers/browser/window-height.ts
@@ -1,19 +1,19 @@
 const WINDOW_HEIGHT_CSS_VAR = '--window-height'
 
-export function useWindowHeightVariable() {
-  function setWindowHeightVariable() {
+export function useWindowHeightCssVariable() {
+  function setWindowHeightCssVariable() {
     document.documentElement.style.setProperty(
       WINDOW_HEIGHT_CSS_VAR,
       `${window.innerHeight / 100}px`,
     )
   }
 
-  setWindowHeightVariable()
-  window.addEventListener('resize', setWindowHeightVariable)
-  window.addEventListener('orientationchange', setWindowHeightVariable)
+  setWindowHeightCssVariable()
+  window.addEventListener('resize', setWindowHeightCssVariable)
+  window.addEventListener('orientationchange', setWindowHeightCssVariable)
 
   return function dispose() {
-    window.removeEventListener('resize', setWindowHeightVariable)
-    window.removeEventListener('orientationchange', setWindowHeightVariable)
+    window.removeEventListener('resize', setWindowHeightCssVariable)
+    window.removeEventListener('orientationchange', setWindowHeightCssVariable)
   }
 }
