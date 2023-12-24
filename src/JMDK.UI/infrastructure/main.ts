2abe530545f6fe77786670f5478b587d628ee74c
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2023-12-24 13:05:33 +0100

    Refactored main.ts to use chaining for a cleaner syntax.

--- src/JMDK.UI/infrastructure/main.ts
+++ src/JMDK.UI/infrastructure/main.ts
@@ -2,8 +2,4 @@ import { createApp } from 'vue'
 import App from '@/JMDK.UI/components/app/App.vue'
 import router from '@/JMDK.Core/router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
