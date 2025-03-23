1f48b268b2d4a36ed8b58b5af49cb3b2858c1bde
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2025-03-23 18:41:14 +0100

    Removed reactive framework 🧼

--- src/JMDK.UI/infrastructure/main.ts
+++ src/JMDK.UI/infrastructure/main.ts
@@ -1,5 +1,1 @@
-import { createApp } from 'vue'
-import App from '@/JMDK.UI/components/app/App.vue'
-import router from '@/JMDK.Core/router'
-
-createApp(App).use(router).mount('#app')
+import '../assets/styles/index.css'
